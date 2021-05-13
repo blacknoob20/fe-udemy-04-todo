34a40d196915948f5e82a658976f388e0b6d9e9f
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-13 11:27:15 -0500

    Agregando elementos al Todo.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './styles.css';
 
 import { Todo, TodoList } from "./classes";
-
+import { crearTodoHtml } from "./js/componentes";
 const todoList = new TodoList();
 const tarea = new Todo('Aprender Javascript');
 
 todoList.nuevoTodo(tarea);
 
 // console.log(tarea); 
-console.log(todoList); 
+console.log(todoList);
+
+crearTodoHtml(tarea);
