3aff7b9d5f2f4ba985f5a4764626f39e414226f0
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-14 11:15:57 -0500

    Aplicando funcionalidad a los botones inferiores del Todo

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -2,9 +2,11 @@ import { Todo } from "../classes";
 import { todoList } from "../index";
 
 // Referencias en el HTML
-const divTodoList = document.querySelector('.todo-list');
-const txtInput    = document.querySelector('.new-todo') ;
-const btnBorrar   = document.querySelector('.clear-completed') ;
+const divTodoList   = document.querySelector('.todo-list');
+const txtInput      = document.querySelector('.new-todo') ;
+const btnBorrar     = document.querySelector('.clear-completed') ;
+const ulFiltros     = document.querySelector('.filters') ;
+const anchorFiltros = document.querySelectorAll('.filtro') ;
 
 export const crearTodoHtml = (todo) => {
     const div = document.createElement('div');
@@ -58,6 +60,34 @@ btnBorrar.addEventListener('click', () => {
         
         if (element.classList.contains('completed')) {
             divTodoList.removeChild(element);
+        }
+    }
+});
+
+ulFiltros.addEventListener('click', (evt) =>{
+    const filtro = evt.target.text;
+
+    if(!filtro){ return; }
+
+    anchorFiltros.forEach(elm => elm.classList.remove('selected'));
+
+    for (const elemento of divTodoList.children) {
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch (filtro) {
+            case 'Pendientes':
+                if (completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
+                
+            case 'Completados':
+                if (!completado) {
+                    elemento.classList.add('hidden');
+                }
+            default:
+                break;
         }
     }
 });
