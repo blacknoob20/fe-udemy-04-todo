8ed179c1cfb631ccd55cc72e9dd50040f6211a56
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-14 10:05:50 -0500

    Recontruyendo instancias de Todo

--- src/classes/todo.class.js
+++ src/classes/todo.class.js
@@ -1,4 +1,16 @@
+import { crearTodoHtml } from "../js/componentes";
+
 export class Todo{
+    static fromJson({id, tarea, completado, creado}){
+        const tempTodo = new Todo(tarea);
+
+        tempTodo.id         = id;
+        tempTodo.completado = completado;
+        tempTodo.creado     = creado;
+
+        return tempTodo;
+    }
+
     constructor(tarea){
         this.tarea      = tarea;
         this.id         = new Date().getTime();
