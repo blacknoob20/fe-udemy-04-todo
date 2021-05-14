7eee75e72c7307da4ae5e8eb5a322fe6f5eab30b
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-14 09:55:27 -0500

    Guardando y recuperando Todo

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import { Todo, TodoList } from "./classes";
 import { crearTodoHtml } from "./js/componentes";
 
 export const todoList = new TodoList();
-const tarea = new Todo('Aprender Javascript');
 
-todoList.nuevoTodo(tarea);
-
-// console.log(tarea); 
-console.log(todoList);
-
-crearTodoHtml(tarea);
+// todoList.todos.forEach(todo => crearTodoHtml( todo ));
+todoList.todos.forEach(crearTodoHtml);
